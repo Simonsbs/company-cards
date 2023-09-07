c55058e3ab1fa3455092b70db2d49b956d237c35
Simon B.Stirling <simonsbs@gmail.com>
2023-09-07 14:28:59 +0300

    changed api calls to use then chains not async

--- src/services/api.js
+++ src/services/api.js
@@ -5,65 +5,84 @@ const api = axios.create({
   baseURL: BaseAPI,
 });
 
-// Login
-export const loginUser = async (email, password) => {
-  const response = await api.post(`/login/${ProjectID}`, {
-    Email: email,
-    Password: password,
-  });
-  return response.data;
+export const loginUser = (email, password) => {
+  return api
+    .post(`/login/${ProjectID}`, {
+      Email: email,
+      Password: password,
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error logging in:", error);
+      throw error;
+    });
 };
 
-// Register User
-export const registerUser = async (email, password, name) => {
-  const response = await api.post("/user/", {
-    ProjectID: ProjectID,
-    Email: email,
-    Password: password,
-    Role: "Guest",
-    Name: name,
-  });
-  return response.data;
-};
-
-// Register User
-export const updateUser = async (token, email, password, name) => {
-  const response = await api.put(
-    `/user/${ProjectID}`,
-    {
+export const registerUser = (email, password, name) => {
+  return api
+    .post("/user/", {
       ProjectID: ProjectID,
       Email: email,
       Password: password,
       Role: "Guest",
       Name: name,
-    },
-    {
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error registering user:", error);
+      throw error;
+    });
+};
+
+export const updateUser = (token, email, password, name) => {
+  return api
+    .put(
+      `/user/${ProjectID}/${email}`,
+      {
+        ProjectID: ProjectID,
+        Email: email,
+        Password: password,
+        Role: "Guest",
+        Name: name,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error updating user:", error);
+      throw error;
+    });
+};
+
+export const getUser = (email) => {
+  return api
+    .get(`/user/object/${ProjectID}/${email}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error getting user:", error);
+      throw error;
+    });
+};
+
+export const postItem = (token, itemCategory, data) => {
+  return api
+    .post(`/item/${ProjectID}_${itemCategory}`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    }
-  );
-  return response.data;
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error posting item:", error);
+      throw error;
+    });
 };
 
-// Get single user
-export const getUser = async (email) => {
-  const response = await api.get(`/user/object/${ProjectID}/${email}`);
-  return response.data;
-};
-
-// Post Item
-export const postItem = async (token, itemCategory, data) => {
-  const response = await api.post(`/item/${ProjectID}_${itemCategory}`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
-};
-
-// Get all items for a project
-export const getItems = async (token, itemCategory) => {
+export const getItems = (token, itemCategory) => {
   const config = token
     ? {
         headers: {
@@ -71,33 +90,40 @@ export const getItems = async (token, itemCategory) => {
         },
       }
     : {};
-  const response = await api.get(`/item/${ProjectID}_${itemCategory}`, config);
-  return response.data;
+
+  return api
+    .get(`/item/${ProjectID}_${itemCategory}`, config)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error getting items:", error);
+      throw error;
+    });
 };
 
-// Update item
-export const updateItem = async (token, itemCategory, itemId, data) => {
-  const response = await api.put(
-    `/item/${ProjectID}_${itemCategory}/${itemId}`,
-    data,
-    {
+export const updateItem = (token, itemCategory, itemId, data) => {
+  return api
+    .put(`/item/${ProjectID}_${itemCategory}/${itemId}`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    }
-  );
-  return response.data;
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error updating item:", error);
+      throw error;
+    });
 };
 
-// Delete item
-export const deleteItem = async (token, itemCategory, itemId) => {
-  const response = await api.delete(
-    `/item/${ProjectID}_${itemCategory}/${itemId}`,
-    {
+export const deleteItem = (token, itemCategory, itemId) => {
+  return api
+    .delete(`/item/${ProjectID}_${itemCategory}/${itemId}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    }
-  );
-  return response.data;
+    })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error deleting item:", error);
+      throw error;
+    });
 };
